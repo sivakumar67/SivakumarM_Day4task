fb436a04ec33c4974f7eda4ebfd2410b270b8a02
praveenraj100k <23pca144@anjaconline.org>
2024-11-21 02:12:27 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById('input');
+let button = document.getElementById('add');
+let todoList = document.getElementById('todoList');
+
+let todos=[];
+window.onload = ()=>{
+  todos = JSON.parse(localStorage.getItem('todoskey')) || []
+  todos.forEach(todo => {
+    addtodo(todo)
+  });
+}
+
+button.addEventListener("click",()=>{
+  todos.push(input.value)
+  localStorage.setItem('todoskey',JSON.stringify(todos))
+  addtodo(input.value)
+  input.value="";
+})
+
+function addtodo(todo){
+  let para = document.createElement('p')
+  para.innerText = todo
+  todoList.appendChild(para)
+
+  para.addEventListener('click',()=>{
+    para.style.textDecoration='line-through'
+    remove(todo)
+  })
+  para.addEventListener('dblclick',()=>{
+    todoList.removeChild(para)
+    remove(todo)
+  })
+}
+function remove(todo){
+  let index = todos.indexOf(todo)
+  if(index>-1)
+    todos.splice(index,1)
+
+  localStorage.setItem('todoskey',JSON.stringify(todos))
+}
